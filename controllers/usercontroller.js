ea86cf2df8f5b86f2e60be53aa366866e76b67e6
Ajay <ajay@mallow-tech.com>
2023-11-17 11:02:25 +0530

    added the changes

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,152 +1,137 @@
-const User=require('../models/user')
-const bcrypt=require('bcrypt')
-const jwt=require('jsonwebtoken')
-const SECRET_KEY="forgetresetpassword"
-const sendPasswordResetEmail=require('../sendPasswordResetEmail')
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const sendPasswordResetEmail = require('../sendPasswordResetEmail');
 
-//CRETE USER
+const SECRET_KEY = "forgetresetpassword";
 
-const createuser=async(req,res)=>{
-    try{
-        const {fname,email,password,cpassword}=req.body
+const createuser = async (req, res) => {
+  try {
+    const { fname, email, password, cpassword } = req.body;
 
-        const existinguser=await User.findOne({email})
+    const existinguser = await User.findOne({ email });
 
-        if(existinguser){
-            return res.status(404).json({ message: 'Email already registered' })
-        }
-        if(password!=cpassword){
-            return 	res.status(501).send('Passwords do not match!')
-        }
-        const newuser=new User({
-            fname , email  : email   .toLowerCase(), password,cpassword});
-        await newuser.save();
+    if (existinguser) {
+      return res.status(404).json({ message: 'Email already registered' });
+    }
 
-        
+    if (password !== cpassword) {
+      return res.status(501).send('Passwords do not match!');
+    }
+
+    const newuser = new User({
+      fname,
+      email: email.toLowerCase(),
+      password,
+      cpassword
+    });
+
+    await newuser.save();
+
     res.json({ message: 'User registered successfully' });
-
-    }
-    catch(error){
-        console.error('Error:', error);
+  } catch (error) {
+    console.error('Error:', error);
     res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
+const loginuser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(422).json({ message: 'Invalid Credentials' });
     }
 
-}
+    console.log('Hashed Password from Database:', user.password);
+    console.log('Entered Password:', password);
 
-const loginuser=async(req,res)=>{
-    try{
-        const {email,password}=req.body;
+    const matchpassword = await bcrypt.compare(password, user.password);
+    console.log(matchpassword);
 
-        const user=await User.findOne({email})
-        if(!user){
-            return res.status(422).json({message:'Invalid Credentials'})
-        }
-        const matchpassword=await bcrypt.compare(password,user.password)
-
-        if(!matchpassword){
-            return res.status(422).json({message:"invalid password"})
-        }
-
-        const token=jwt.sign({userId:user._id,fname:user.fname,email:user.email},SECRET_KEY,{expiresIn:'1hr'})
-        
-
-        res.json(token);
-
-       
-
-    }
-    catch(error){
-        console.error('Error:', error);
-        res.status(500).json({ message: 'Internal server error' });
-
+    if (!matchpassword) {
+      return res.status(422).json({ message: 'Invalid password' });
     }
 
-}
+    const token = jwt.sign(
+      { userId: user._id, fname: user.fname, email: user.email },
+      SECRET_KEY,
+      { expiresIn: '1hr' }
+    );
 
-const userprofile=async(req,res)=>{
-    try{
-        const userId=req.userId;
-        const user=await User.findById(userId,'fname email')
-        res.json(user)
-    }catch(error){
-        console.error('error fetching user profile',error)
-        res.status(500).json({message:"internal server error"})
+    res.json(token);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
+
+const userprofile = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId, 'fname email');
+    res.json(user);
+  } catch (error) {
+    console.error('error fetching user profile', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const forgetpassword = async (req, res) => {
+  const { email } = req.body;
+  console.log(email)
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
     }
 
-}
+    const resetToken = jwt.sign({ email }, SECRET_KEY, { expiresIn: '1h' });
 
-const forgetpassword=async(req,res)=>{
-    // Add this route to initiate password reset
+    user.resetPasswordToken = resetToken;
+    user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
 
-    const { email } = req.body;
-  
-    try {
-      const user = await User.findOne({ email });
-  
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      // Generate a reset token and set an expiration time
-      const resetToken = jwt.sign({ email }, "forgetresetpassword", { expiresIn: '1h' });
-    //   console.log(resetToken)
-      user.resetPasswordToken = resetToken;
-      user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-  
-      await user.save();
+    await user.save();
 
-  
-      // Send an email with the reset link (implement this function)
-      sendPasswordResetEmail(email, resetToken);
-  
-      res.json(resetToken);
-    } catch (error) {
-      console.error('Error sending password reset email:', error);
-      res.status(500).json({ message: 'Internal server error' });
+    sendPasswordResetEmail(email, resetToken);
+
+    res.json(resetToken);
+  } catch (error) {
+    console.error('Error sending password reset email:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const resetpassword = async (req, res) => {
+  const { token } = req.params;
+  const { newPassword } = req.body;
+
+  try {
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid or expired token' });
     }
 
-  
-}
+    user.password = newPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
 
+    await user.save();
 
-const resetpassword=async(req,res)=>{
-    // Add this route to reset the password using the token
+    res.json({ message: 'Password reset successfully' });
+  } catch (error) {
+    console.error('Error resetting password:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
-    const { token } = req.params;
-    const { newPassword } = req.body;
-  
-    try {
-      const user = await User.findOne({
-        resetPasswordToken: token,
-        resetPasswordExpires: { $gt: Date.now() },
-      });
-  
-      if (!user) {
-        return res.status(400).json({ message: 'Invalid or expired token' });
-      }
-  
-      
-      user.password = await bcrypt.hash(newPassword, 10);
-      user.resetPasswordToken = undefined;
-      user.resetPasswordExpires = undefined;
-  
-      await user.save();
-  
-      res.json({ message: 'Password reset successfully' });
-    } catch (error) {
-      console.error('Error resetting password:', error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
- 
-  
-
-}
-
-module.exports={createuser,loginuser,userprofile,forgetpassword,resetpassword}
-
-
-
-
-
+module.exports = { createuser, loginuser, userprofile, forgetpassword, resetpassword };
--- models/user.js
+++ models/user.js
@@ -1,63 +1,46 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt=require('bcrypt')
+const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 
-
-
-
 const userSchema = new mongoose.Schema({
-    fname: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("not valid email")
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6
-    },
-    cpassword: {
-        type: String,
-        required: true,
-        minlength: 6
-    },
-    resetPasswordToken: String,
-    resetPasswordExpires: Date,
-   
+  fname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("not valid email");
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  cpassword: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
 });
-
-
-
-
-
-
 
 userSchema.pre("save", async function (next) {
-
-    if (this.isModified("password")) {
-        this.password = await bcrypt.hash(this.password, 12);
-        this.cpassword = await bcrypt.hash(this.cpassword, 12);
-    }
-    next()
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+    this.cpassword = await bcrypt.hash(this.cpassword, 10);
+  }
+  next();
 });
 
+const User = mongoose.model("users", userSchema);
 
-
-
-
-
-const user = new mongoose.model("users", userSchema);
-
-module.exports = user;
-
+module.exports = User;
